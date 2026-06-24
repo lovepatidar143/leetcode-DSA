28a7832b5655a631095fbfa053744e3f37ecadeb
love patidar <engrlovepatidar@gmail.com>
2026-06-24 09:08:30 +0530

    Time: 207 ms (21.74%), Space: 13.9 MB (59.33%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int solve(int i , int j , vector <int>&arr , vector<vector<int>> &dp){
         if(i>j) return 0 ; 
-        if(i == j) return arr[i]*arr[i-1]*arr[i+1]  ; 
+        // if(i == j) return arr[i]*arr[i-1]*arr[i+1]  ; 
         if(dp[i][j] != -1) return dp[i][j] ; 
         int coins = -1e9 ; 
         for(int  k= i ; k <= j; k++){
