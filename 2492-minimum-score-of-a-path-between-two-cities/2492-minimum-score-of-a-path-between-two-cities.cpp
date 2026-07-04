5f3bdee6b27e547686c6d419b293120ad6591564
love patidar <engrlovepatidar@gmail.com>
2026-07-04 10:09:54 +0530

    Time: 97 ms (37.05%), Space: 145.8 MB (19.86%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> adj(n+1) ; 
+        vector<vector<pair <int, int >>> adj (n+1) ; 
         for(auto it : roads){
-            //bidectional road
-            adj[it[0]].push_back({it[1] , it[2]}) ; 
-            adj[it[1]].push_back({it[0] , it[2]}) ; 
+            adj[it[0]].push_back({it[1] , it[2]}) ;
+            adj[it[1]].push_back({it[0] , it[2]}) ;
         }
-        // adjecenty list is done ,
-        using P = pair <int , int > ; // node , dist
-        priority_queue < P , vector<P> , greater<>> pq ; 
-        pq.push({INT_MAX , 1}) ; 
+        // now find who who is connected to the 1 and n 
+        vector<int> visited(n+1 , 0) ; 
+        queue < int > q ; 
+        q.push(1) ; 
+        visited[1] = 1 ; 
         int ans = INT_MAX ; 
-        vector<int> distance(n+1 , INT_MAX); 
-        // distance[1] = INT_MAX ; 
-        while(!pq.empty()){
-            auto [dist , node] = pq.top() ; 
-            pq.pop() ; 
-            if(node == n) {
-                ans = min(ans , dist) ; 
-            }
+        while(!q.empty()){
+            int node = q.front() ; 
+            q.pop();
             for(auto it : adj[node]){
-                int newNode = it.first ;
-                int value = min(it.second , dist)  ; 
-                if(distance[newNode] > value){
-                    distance[newNode] = value  ; 
-                    pq.push({value , newNode}) ;
+                int newNode = it.first ; 
+                ans = min(ans , it.second) ; 
+                if(!visited[newNode]){
+
+                    visited[newNode] =1 ; 
+                    q.push(newNode) ; 
                 }
             }
         }
