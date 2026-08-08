9cd42ea2039c9aea3bac0e2ffeac856b15bb5a0c
love patidar <engrlovepatidar@gmail.com>
2026-08-08 20:49:31 +0530

    Time: 53 ms (27.66%), Space: 115.7 MB (17.02%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,27 +1,62 @@
 class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
-        vector<int> last(m, -1);
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<int> matched(n, 0);
+
+        int match = 0;
+        int i = n - 1;
         int j = m - 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (j >= 0 && word1[i] == word2[j]) {
-                last[j] = i;
-                j -= 1;
+
+        // matched[i] = how many characters of word2
+        // can be matched from word1[i...n-1]
+        while (i >= 0 && j >= 0) {
+            if (word1[i] == word2[j]) {
+                match++;
+                j--;
             }
+
+            matched[i] = match;
+            i--;
         }
-        vector<int> res;
-        int skip = 0;
+
+        while (i >= 0) {
+            matched[i] = match;
+            i--;
+        }
+
+        vector<int> ans;
+
+        bool canMismatch = true;
+
+        i = 0;
         j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (j == m) break;
-            if (word1[i] == word2[j] ||
-                (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
-                skip += (word1[i] != word2[j] ? 1 : 0);
-                res.push_back(i);
-                j += 1;
+
+    
+        while (i < n && j < m) {
+
+            // Exact match
+            if ( word1[i] == word2[j]) {
+                ans.push_back(i);
+                j++;
             }
+
+
+            // Use our one allowed mismatch
+            else if (canMismatch && i < n-1 && matched[i+1] >= m - j - 1) {
+                ans.push_back(i);
+                canMismatch = false;
+                j++;
+            }
+
+            i++;
         }
-        return j == m ? res : vector<int>();
+
+        if (j != m)
+            return {};
+
+        return ans;
     }
 };
