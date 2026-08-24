0daead961940133be62cd1a1febe5686836fcc57
love patidar <engrlovepatidar@gmail.com>
2026-08-24 18:30:17 +0530

    Time: 118 ms (25.04%), Space: 102.9 MB (20.5%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int stoneGameVIII(vector<int>& stones) {
-        int n = stones.size();
-        vector<int> pre;
-        partial_sum(stones.begin(), stones.end(), back_inserter(pre));
-        vector<int> f(n);
-        f[n - 1] = pre[n - 1];
-        for (int i = n - 2; i >= 1; --i) {
-            f[i] = max(f[i + 1], pre[i] - f[i + 1]);
-        }
-        return f[1];
+    int solve(int i , vector<int> &pre, vector<int> &dp){
+        int n = pre.size() ; 
+        //base condition 
+        if(i == n-1) return dp[i] =  pre[i] ; 
+        if(dp[i] != INT_MIN) return dp[i] ; 
+        int take , nottake ; 
+        take = pre[i]  - solve(i +1 , pre , dp);  
+        nottake = solve(i +1 , pre , dp) ; 
+        return dp[i] = max(take , nottake) ; 
     }
-};
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size() ; 
+        vector<int> pre(n , 0) ; 
+        pre[0] = stones[0] ; 
+        for(int i = 1 ; i< n ; i++) pre[i] = pre[i-1] + stones[i] ; 
+        vector<int> dp( n , INT_MIN ) ;  
+        return solve(1 , pre, dp) ; 
+    }
+}; 
