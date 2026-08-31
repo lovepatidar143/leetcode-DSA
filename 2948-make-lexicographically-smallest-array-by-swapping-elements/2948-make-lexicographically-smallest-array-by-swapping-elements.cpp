94b29d542355f9e19d6c9c17fe582b1896db5833
love patidar <engrlovepatidar@gmail.com>
2026-08-31 22:14:12 +0530

    Time: 1492 ms (5.01%), Space: 180.5 MB (52.65%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,42 +1,58 @@
+class DSU{
+    vector<int> rank ; 
+    public : 
+    vector<int> parent ; 
+    DSU(int n ){
+        parent.resize(n) ; 
+        rank.resize(n , 1) ; 
+        for(int i = 0 ; i< n ; i++) parent[i] = i ; 
+
+    }
+
+    int find(int i ){
+        if(parent[i] == i) return i ; 
+        return parent[i] = find(parent[i]);
+    }
+    void join(int u , int v) {
+        int pu = find(u) ; 
+        int pv = find(v) ; 
+        if(rank[pu] < rank[pv]){
+            parent[pu]  = pv ; 
+        }
+        else if(rank[pu] > rank[pv]) {
+            parent[pv] = pu ; 
+        }
+        else {
+            parent[pu] = pv ; 
+            rank[pv]++;
+        }
+    }
+
+};
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        vector<int> numsSorted(nums);
-        sort(numsSorted.begin(), numsSorted.end());
-
-        int currGroup = 0;
-        unordered_map<int, int> numToGroup;
-        numToGroup.insert(pair<int, int>(numsSorted[0], currGroup));
-
-        unordered_map<int, list<int>> groupToList;
-        groupToList.insert(
-            pair<int, list<int>>(currGroup, list<int>(1, numsSorted[0])));
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
-                // new group
-                currGroup++;
-            }
-
-            // assign current element to group
-            numToGroup.insert(pair<int, int>(numsSorted[i], currGroup));
-
-            // add element to sorted group list
-            if (groupToList.find(currGroup) == groupToList.end()) {
-                groupToList[currGroup] = list<int>();
-            }
-            groupToList[currGroup].push_back(numsSorted[i]);
+        int n = nums.size() ;
+        DSU  dsu(n)  ;
+        vector<int> ans ; 
+        vector<pair<int, int >> arr; 
+        for(int i = 0 ; i< n;  i++){
+            arr.push_back({nums[i] , i }) ; 
         }
-
-        // iterate through input and overwrite each element with the next
-        // element in its corresponding group
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            int group = numToGroup[num];
-            nums[i] = *groupToList[group].begin();
-            groupToList[group].pop_front();
+        sort(arr.begin() , arr.end()) ; 
+        for(int i = 1 ; i< n ; i++){
+            if(arr[i].first - arr[i-1].first <= limit) dsu.join(arr[i].second , arr[i-1].second) ;
         }
-
-        return nums;
+        unordered_map < int , vector<int>> hash ; 
+        for(int i = 0 ; i< n ; i++){
+            hash[dsu.find(i)].push_back(nums[i]);
+        }
+        for(auto &it : hash) sort(it.second.begin() ,it.second.end());
+        for(int i = 0 ; i < n ; i++){
+            int x = dsu.find(i) ; 
+            ans.push_back(hash[x][0]) ; 
+            hash[x].erase(hash[x].begin());
+        }
+        return ans ; 
     }
 };
